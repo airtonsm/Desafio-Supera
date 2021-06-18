33973d6d902d608a763984aa2ad998a29a740aa8
airtonsm <airtonsmorais28@gmail.com>
2021-06-18 19:41:28 -0300

    project created

---
+++ src/main/java/com/airton/desafiosupera/DesafioSuperaApplication.java
@@ -0,0 +1,13 @@
+package com.airton.desafiosupera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSuperaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioSuperaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/airton/desafiosupera/DesafioSuperaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airton.desafiosupera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSuperaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
